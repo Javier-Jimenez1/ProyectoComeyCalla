b6f443e371129adccc03209c4c1e902b5cd6671b
Javier Jimenez <jjimenezjimenez@safareyes.es>
2025-04-10 20:14:24 +0200

    intento de conectar con la base de datos jeje

--- ProyectoComeyCalla/settings.py
+++ ProyectoComeyCalla/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'ProyectoComeyCalla.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.oracle',
+        'NAME': 'XE',
+        'USER': 'C##PROYECTO',
+        'PASSWORD': 'comeycalla',
+        'HOST': 'localhost',
+        'PORT': '1521',
     }
 }
 
