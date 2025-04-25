4b0b25b32bb9fa1b11b5c2665855dc0f00121b41
Javier Jimenez <jjimenezjimenez@safareyes.es>
2025-04-25 16:58:21 +0200

    cambios de nombres clases

--- restauranteapp/models.py
+++ restauranteapp/models.py
@@ -45,7 +45,7 @@
 # # --- BASE DE DATOS --- #
 # class Categoria(models.Model):
 #     nombre = models.CharField(max_length=50, unique=True)
-#     icono = models.CharField(max_length=30, blank=True)  # Ej: 'fa-utensils' para FontAwesome
+#     icono = models.CharField(max_length=30, blank=True)
 #
 #     def __str__(self):
 #         return self.nombre
